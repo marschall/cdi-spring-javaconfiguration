b83749ddea3fa5a0c22ffb093733c6c2d97d0554
Philippe Marschall <philippe.marschall@gmail.com>
2013-09-23 16:35:41 -0700

    fix tests

--- src/test/java/com/github/marschall/cdispringjavaconfig/TwoElementSetTest.java
+++ src/test/java/com/github/marschall/cdispringjavaconfig/TwoElementSetTest.java
@@ -1,11 +1,16 @@
 package com.github.marschall.cdispringjavaconfig;
 
-import static org.hamcrest.Matchers.contains;
+import static org.hamcrest.Matchers.hasItem;
 import static org.hamcrest.Matchers.hasSize;
 import static org.hamcrest.Matchers.not;
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertThat;
 import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
+
+import java.util.Iterator;
+import java.util.NoSuchElementException;
 
 import org.junit.Test;
 
@@ -21,9 +26,40 @@ public class TwoElementSetTest {
     assertFalse(set.contains("third"));
     
     // uses iterator
-    assertThat(set, contains("first"));
-    assertThat(set, contains("second"));
-    assertThat(set, not(contains("second")));
+    assertThat(set, hasItem("first"));
+    assertThat(set, hasItem("second"));
+    assertThat(set, not(hasItem("third")));
+  }
+  
+  @Test
+  public void iteratorRemove() {
+    TwoElementSet<String> set = new TwoElementSet<>("first", "second");
+    Iterator<String> iterator = set.iterator();
+    try {
+      iterator.remove();
+      fail("iterator should not support remove");
+    } catch (UnsupportedOperationException e) {
+      // should reach here
+    }
+  }
+  
+  @Test
+  public void iterator() {
+    TwoElementSet<String> set = new TwoElementSet<>("first", "second");
+    Iterator<String> iterator = set.iterator();
+    assertTrue(iterator.hasNext());
+    assertEquals("first", iterator.next());
+    
+    assertTrue(iterator.hasNext());
+    assertEquals("second", iterator.next());
+    
+    assertFalse(iterator.hasNext());
+    try {
+      iterator.next();
+      fail("iterator should be at end");
+    } catch (NoSuchElementException e) {
+      // should reach here
+    }
   }
 
 }
